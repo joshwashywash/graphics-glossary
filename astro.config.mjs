dbd4d8e2c47a01cac0fc526066c642693b2c2885
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2025-05-14 10:45:23 -0500

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,36 +8,40 @@ import svelte from "@astrojs/svelte";
 
 import sitemap from "@astrojs/sitemap";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			customCss: ["./src/styles/global.css"],
-			expressiveCode: {
-				themes: ["rose-pine", "rose-pine-dawn"],
-			},
-			title: "3d graphics glossary",
-			social: [
-				{
-					icon: "github",
-					label: "GitHub",
-					href: "https://github.com/withastro/starlight",
-				},
-			],
-			sidebar: [
-				{
-					label: "Guides",
-					autogenerate: {
-						directory: "guides",
-					},
-				},
-			],
-		}),
-		svelte(),
-		sitemap(),
+  integrations: [
+      starlight({
+          customCss: ["./src/styles/global.css"],
+          expressiveCode: {
+              themes: ["rose-pine", "rose-pine-dawn"],
+          },
+          title: "3d graphics glossary",
+          social: [
+              {
+                  icon: "github",
+                  label: "GitHub",
+                  href: "https://github.com/withastro/starlight",
+              },
+          ],
+          sidebar: [
+              {
+                  label: "Guides",
+                  autogenerate: {
+                      directory: "guides",
+                  },
+              },
+          ],
+      }),
+      svelte(),
+      sitemap(),
 	],
-	vite: {
-		plugins: [tailwindcss()],
-	},
-});
 
+  vite: {
+      plugins: [tailwindcss()],
+	},
+
+  adapter: vercel(),
+});
