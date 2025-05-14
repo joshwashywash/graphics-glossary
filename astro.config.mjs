1686a5162bcdf01417127e5d3ff719704cffc4eb
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2025-05-14 11:25:55 -0500

    change github link

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,36 +12,37 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-      starlight({
-          customCss: ["./src/styles/global.css"],
-          expressiveCode: {
-              themes: ["rose-pine", "rose-pine-dawn"],
-          },
-          title: "3d graphics glossary",
-          social: [
-              {
-                  icon: "github",
-                  label: "GitHub",
-                  href: "https://github.com/withastro/starlight",
-              },
-          ],
-          sidebar: [
-              {
-                  label: "Guides",
-                  autogenerate: {
-                      directory: "guides",
-                  },
-              },
-          ],
-      }),
-      svelte(),
-      sitemap(),
+	integrations: [
+		starlight({
+			customCss: ["./src/styles/global.css"],
+			expressiveCode: {
+				themes: ["rose-pine", "rose-pine-dawn"],
+			},
+			title: "3d graphics glossary",
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/joshwashywash/graphics-glossary",
+				},
+			],
+			sidebar: [
+				{
+					label: "Guides",
+					autogenerate: {
+						directory: "guides",
+					},
+				},
+			],
+		}),
+		svelte(),
+		sitemap(),
 	],
 
-  vite: {
-      plugins: [tailwindcss()],
+	vite: {
+		plugins: [tailwindcss()],
 	},
 
-  adapter: vercel(),
+	adapter: vercel(),
 });
+
