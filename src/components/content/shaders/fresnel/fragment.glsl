28b8d0a3d037f85768e53f06a208cf0e84d952e5
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2025-05-28 16:53:52 -0500

    add comments to fresnel fragment shader

--- src/components/content/shaders/fresnel/fragment.glsl
+++ src/components/content/shaders/fresnel/fragment.glsl
@@ -2,7 +2,8 @@ varying vec3 vNormal;
 varying vec3 vPosition;
 
 void main() {
-    vec3 toCamera = normalize(cameraPosition - vPosition);
-    float fresnel = 1.0 - 0.5 * ((dot(toCamera, vNormal) + 1.0));
+    vec3 toCameraNormalized = normalize(cameraPosition - vPosition);
+    // dot product's range is -1 to 1. remap to 0 -> 1 then invert so that the edges are white
+    float fresnel = 1.0 - 0.5 * ((dot(toCameraNormalized, vNormal) + 1.0));
     gl_FragColor = vec4(fresnel, fresnel, fresnel, 1.0);
 }
