e3223735b77eef65a51a285363fef1c964b33b16
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2025-06-01 19:12:44 -0500

    inline renderer attachment default export

--- src/attachments/renderer.svelte.ts
+++ src/attachments/renderer.svelte.ts
@@ -6,7 +6,7 @@ export type Setup = (renderer: WebGLRenderer) => (() => void) | void;
 /**
  * @param setup a function that will be called with the created renderer. it may return a 'cleanup' function that will be called when the canvas is removed
  */
-const renderer = (setup?: Setup): Attachment<HTMLCanvasElement> => {
+export default (setup?: Setup): Attachment<HTMLCanvasElement> => {
 	return (canvas) => {
 		const renderer = new WebGLRenderer({
 			antialias: true,
@@ -20,5 +20,3 @@ const renderer = (setup?: Setup): Attachment<HTMLCanvasElement> => {
 		return renderer.dispose;
 	};
 };
-
-export default renderer;
