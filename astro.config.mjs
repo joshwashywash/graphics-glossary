0788365cf3809770eeddd63a17bdf8c9c86740cb
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2025-10-14 09:39:48 -0500

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
-import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
 import starlight from "@astrojs/starlight";
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
 
 const site = "https://joshwashywash.github.io";
 
@@ -12,72 +13,71 @@ const repoURL = `https://github.com/joshwashywash/graphics-glossary`;
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			customCss: ["./src/styles/global.css"],
-			editLink: {
-				baseUrl: `${repoURL}/edit/main`,
-			},
-			expressiveCode: {
-				themes: ["rose-pine", "rose-pine-dawn"],
-			},
-			sidebar: [
-				{
-					label: "Data Buffers",
-					autogenerate: {
-						directory: "data-buffers",
-					},
-				},
-				{
-					label: "Shaders",
-					autogenerate: {
-						directory: "shaders",
-					},
-				},
-				{
-					label: "Shadows",
-					autogenerate: {
-						directory: "shadows",
-					},
-				},
-				{
-					label: "Techniques",
-					autogenerate: {
-						directory: "techniques",
-					},
-				},
-				{
-					label: "Miscellaneous",
-					autogenerate: {
-						directory: "miscellaneous",
-					},
-				},
-			],
-			social: [
-				{
-					icon: "github",
-					label: "GitHub",
-					href: repoURL,
-				},
-			],
-			title: "3d graphics glossary",
-		}),
-		svelte({
-			compilerOptions: {
-				experimental: {
-					async: true,
-				},
-			},
-		}),
-		sitemap(),
+  integrations: [
+      starlight({
+          customCss: ["./src/styles/global.css"],
+          editLink: {
+              baseUrl: `${repoURL}/edit/main`,
+          },
+          expressiveCode: {
+              themes: ["rose-pine", "rose-pine-dawn"],
+          },
+          sidebar: [
+              {
+                  label: "Data Buffers",
+                  autogenerate: {
+                      directory: "data-buffers",
+                  },
+              },
+              {
+                  label: "Shaders",
+                  autogenerate: {
+                      directory: "shaders",
+                  },
+              },
+              {
+                  label: "Shadows",
+                  autogenerate: {
+                      directory: "shadows",
+                  },
+              },
+              {
+                  label: "Techniques",
+                  autogenerate: {
+                      directory: "techniques",
+                  },
+              },
+              {
+                  label: "Miscellaneous",
+                  autogenerate: {
+                      directory: "miscellaneous",
+                  },
+              },
+          ],
+          social: [
+              {
+                  icon: "github",
+                  label: "GitHub",
+                  href: repoURL,
+              },
+          ],
+          title: "3d graphics glossary",
+      }),
+      svelte({
+          compilerOptions: {
+              experimental: {
+                  async: true,
+              },
+          },
+      }),
+      sitemap(),
 	],
 
-	site,
+  site,
 
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
 
-	adapter: netlify(),
+  adapter: cloudflare(),
 });
-
